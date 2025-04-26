da674962d3478162b2e850760976e3be74ecc909
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-26 18:39:19 +0200

    ADD: - Thumbnail component for displaying video thumbnails in the VideoCard component.

---
+++ src/components/shared/videoCard/Thumbnail.tsx
@@ -0,0 +1,20 @@
+import { Image } from 'expo-image';
+import { View } from 'react-native';
+
+import { blurhash } from '@/core/settings/image';
+
+import type { ThumbnailProps } from './types';
+
+export function Thumbnail({ item }: ThumbnailProps) {
+  return (
+    <View className="w-full flex-[5]">
+      <Image
+        style={{ width: '100%', height: '100%', borderRadius: 10 }}
+        source={{ uri: item.snippet.thumbnails.medium.url }}
+        placeholder={{ blurhash }}
+        contentFit="cover"
+        transition={300}
+      />
+    </View>
+  );
+}
