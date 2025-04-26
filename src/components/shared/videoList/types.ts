3ce9bca88889d1c055ea63ae7fbc7a704aae95c5
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-26 21:24:16 +0200

    UPDATE: - Added contentContainerStyle property to horizontal and vertical VideoListProps types

--- src/components/shared/videoList/types.ts
+++ src/components/shared/videoList/types.ts
@@ -5,9 +5,11 @@ import type { Item } from '@/core/api/services/search';
 export type VideoListProps = {
   horizontal?: {
     estimatedItemSize?: number;
+    contentContainerStyle?: object;
   };
   vertical?: {
     estimatedItemSize?: number;
+    contentContainerStyle?: object;
   };
   data: Item[];
   renderItem: ListRenderItem<Item>;
