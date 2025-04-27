5d5cc62a4ac4f0cf787f1dad693057f65ba5efb0
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-28 01:24:48 +0200

    ADD: - component for video player

---
+++ src/features/video-player/components/ProgressBar.tsx
@@ -0,0 +1,46 @@
+import { Pressable, View } from 'react-native';
+
+import { useProgressBar } from '../hooks/useProgressBar';
+import type { ProgressBarProps } from '../types/component-types';
+
+export function ProgressBar({ handleSeek }: ProgressBarProps) {
+  const {
+    progressPercentage,
+    playableProgressPercentage,
+    dotPosition,
+    progressBarRef,
+    panResponder,
+    handleOnLayout,
+  } = useProgressBar({ handleSeek });
+  return (
+    <View
+      className="h-2 w-full items-start justify-center bg-black"
+      ref={progressBarRef}
+      onLayout={handleOnLayout}
+      {...panResponder.panHandlers}
+    >
+      <View
+        className="h-2 w-full bg-gray-500"
+        style={{ width: playableProgressPercentage, zIndex: 1 }}
+      />
+      <View
+        className="h-2 w-full bg-red-500"
+        style={{
+          width: progressPercentage,
+          zIndex: 2,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+        }}
+      />
+      <Pressable
+        className="absolute left-0 top-0 size-[12px] rounded-full bg-red-500"
+        style={{
+          left: dotPosition,
+          transform: [{ translateX: -5 }, { translateY: -2.5 }],
+          zIndex: 3,
+        }}
+      />
+    </View>
+  );
+}
