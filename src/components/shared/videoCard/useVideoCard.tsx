fcf4767c46371ae470fa9fc0853625ac4f2a9441
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-27 15:26:29 +0200

    UPDATE: - useVideoCard hook to include onPress functionality for item interaction

--- src/components/shared/videoCard/useVideoCard.tsx
+++ src/components/shared/videoCard/useVideoCard.tsx
@@ -1,8 +1,12 @@
 import type { VideoCardProps } from './types';
 
-export function useVideoCard({ item }: Omit<VideoCardProps, 'style'>) {
+export function useVideoCard({ item, onPress }: Omit<VideoCardProps, 'style'>) {
   const { publishedAt, title, channelTitle } = item.snippet;
   const publishedAtDate = new Date(publishedAt).toLocaleDateString();
 
-  return { publishedAtDate, title, channelTitle };
+  const handlePress = () => {
+    onPress?.(item);
+  };
+
+  return { publishedAtDate, title, channelTitle, handlePress };
 }
