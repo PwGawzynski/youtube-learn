46b6a61003352a09bbb99aba233dda4f4da19899
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-27 15:20:00 +0200

    UPDATE: - Moved from feed due to restrictedZones

--- src/app/(tab)/index.tsx
+++ src/app/(tab)/index.tsx
@@ -1,14 +1,34 @@
-import { SafeAreaView, View } from 'react-native';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { SafeAreaView, ScrollView, View } from 'react-native';
 
-import { Feed } from '@/features/feed/components/Feed';
-import { SearchAndSettingsHeader } from '@/features/search-and-settings-header/components/SearchAndSettingsHeader';
+import { CategorizedList } from '@/features/categorized-list';
+import { SearchAndSettingsHeader } from '@/features/search-and-settings-header';
+
+const categories = ['React Native', 'React', 'TypeScript', 'JavaScript'];
+const MAX_RESULTS = 1;
 
 export default function Index() {
+  const bottomTabHeight = useBottomTabBarHeight();
+  const paddingBottom = bottomTabHeight * 3;
   return (
     <View className="flex-1">
       <SafeAreaView className="mt-12 flex-1">
         <SearchAndSettingsHeader />
-        <Feed />
+        <ScrollView
+          className="flex-1"
+          contentContainerStyle={{
+            paddingBottom,
+          }}
+        >
+          {categories.map((category, index) => (
+            <CategorizedList
+              key={category}
+              categoryName={category}
+              maxResults={MAX_RESULTS}
+              showSeparator={index !== categories.length - 1}
+            />
+          ))}
+        </ScrollView>
       </SafeAreaView>
     </View>
   );
