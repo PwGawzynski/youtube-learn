c9ec99f14c02911ad83e97d3729aedac6e307ac6
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-27 15:24:45 +0200

    UPDATE: - Header component to accept additional className prop

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,7 +1,10 @@
 import { Text } from '@/components/ui/text';
+import { cn } from '@/lib/utils';
 
 import type { HeaderProps } from './types';
 
-export function Header({ children }: HeaderProps) {
-  return <Text className="text-2xl font-bold">{children}</Text>;
+export function Header({ children, className }: HeaderProps) {
+  return (
+    <Text className={cn('text-2xl font-bold', className)}>{children}</Text>
+  );
 }
