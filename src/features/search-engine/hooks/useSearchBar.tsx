2f10723fc9d42cf8e85ba86e4acde508d5f3ea71
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-27 11:44:38 +0200

    REMOVE: Delete SearchBar component, its associated hook, and TypeScript types for search functionality

--- src/features/search-engine/hooks/useSearchBar.tsx
+++
@@ -1,9 +0,0 @@
-import { useIsDark } from '@/core/hooks/useIsDark';
-
-export function useSearchBar() {
-  const isDark = useIsDark();
-
-  return {
-    isDark,
-  };
-}
--- src/features/search-engine/types/components-types.ts
+++
@@ -1,6 +0,0 @@
-export type SearchBarProps = {
-  placeholder: string;
-  onChangeText: (text: string) => void;
-  value: string;
-  autoFocus?: boolean;
-};
