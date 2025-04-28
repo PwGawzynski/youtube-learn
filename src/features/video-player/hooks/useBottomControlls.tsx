96d7a8145443069b6e3640fb5c0f7da20a783dbc
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-28 09:31:54 +0200

    REFACTOR: - Simplified useBottomControlls hook by directly manipulating videoPlayerStore

--- src/features/video-player/hooks/useBottomControlls.tsx
+++ src/features/video-player/hooks/useBottomControlls.tsx
@@ -1,15 +1,9 @@
-import { use$ } from '@legendapp/state/react';
-
 import { videoPlayerStore$ } from '@/core/state';
 
 export function useBottomControlls() {
-  const isFullscreen = use$(videoPlayerStore$.isFullscreen);
-  const setIsFullscreen = use$(videoPlayerStore$.setIsFullscreen);
-  const setIsControlPanelAwaked = use$(videoPlayerStore$.setControlPanelAwaked);
-
   const handlePressFullscreen = () => {
-    setIsFullscreen(!isFullscreen);
-    setIsControlPanelAwaked(false);
+    videoPlayerStore$.isFullscreen.set((f) => !f);
+    videoPlayerStore$.controlPanelAwaked.set(false);
   };
 
   return {
