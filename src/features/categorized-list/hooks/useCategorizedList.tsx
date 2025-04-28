b016d9ed8f225ca29a68a47cf7a16fed6baf5211
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-29 01:07:48 +0200

    REFACTOR: - useCategorizedList hook to use VideoInfoItem type

--- src/features/categorized-list/hooks/useCategorizedList.tsx
+++ src/features/categorized-list/hooks/useCategorizedList.tsx
@@ -1,19 +1,21 @@
 import { useRouter } from 'expo-router';
 
-import type { Item } from '@/core/api/services/search';
+import type { VideoInfoItem } from '@/core/api/services/search';
 import { useYtSearchQuery } from '@/core/hooks/useYtSearchQuery';
 import type { VideoScreenLocalParams } from '@/core/types';
 
 import type { UseCategorizedListProps } from '../types/hooks-types';
 
+const DEFAULT_MAX_RESULTS = 5;
+
 export function useCategorizedList({
   categoryName,
-  maxResults = 5,
+  maxResults = DEFAULT_MAX_RESULTS,
 }: UseCategorizedListProps) {
   const router = useRouter();
   const query = useYtSearchQuery({ queryString: categoryName, maxResults });
 
-  const handleVideoPress = (item: Item) => {
+  const handleVideoPress = (item: VideoInfoItem) => {
     const params: VideoScreenLocalParams = {
       videoId: item.id.videoId,
     };
@@ -23,5 +25,12 @@ export function useCategorizedList({
       params,
     });
   };
-  return { ...query, handleVideoPress };
+
+  const handleMorePress = () => {
+    router.push({
+      pathname: '/(auth)/search',
+      params: { query: categoryName },
+    });
+  };
+  return { ...query, handleVideoPress, handleMorePress };
 }
