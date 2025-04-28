833ad839b277bc805a172b114cda13f6e9d26f92
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-28 14:07:40 +0200

    MOVE: - moved to src/utils due to reusage of function

--- src/features/video-player/utils/format-time.ts
+++
@@ -1,9 +0,0 @@
-export function formatTime(seconds: number) {
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
-  const formattedMinutes = minutes.toString().padStart(2, '0');
-  const formattedSeconds = Math.floor(remainingSeconds)
-    .toString()
-    .padStart(2, '0');
-  return `${formattedMinutes}:${formattedSeconds}`;
-}
