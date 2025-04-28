d4220e5b35508d89db474b018a31668ada87f345
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-28 09:33:05 +0200

    REFACTOR: - Enhanced useControllVideoPlayBack hook by adding forward and backward playback functionality and directly manipulating control panel state.

--- src/features/video-player/hooks/useControllVideoPlayBack.tsx
+++ src/features/video-player/hooks/useControllVideoPlayBack.tsx
@@ -2,23 +2,39 @@ import { use$ } from '@legendapp/state/react';
 
 import { videoPlayerStore$ } from '@/core/state';
 
+import { BACKWARD_TIME, FORWARD_TIME } from '../utils/setup';
+
 export function useControllVideoPlayBack() {
   const isPlaying = use$(videoPlayerStore$.isPlaying);
   const setIsPlaying = use$(videoPlayerStore$.setIsPlaying);
-  const setIsControlPanelAwaked = use$(videoPlayerStore$.setControlPanelAwaked);
+  const currentTime = use$(videoPlayerStore$.progresInfo.currentTime);
+  const seek = use$(videoPlayerStore$.seek);
 
   const handlePressPlay = () => {
     setIsPlaying(true);
-    setIsControlPanelAwaked(false);
+    videoPlayerStore$.controlPanelAwaked.set(false);
   };
 
   const handlePressPause = () => {
     setIsPlaying(false);
-    setIsControlPanelAwaked(false);
+    videoPlayerStore$.controlPanelAwaked.set(false);
   };
+
+  const handlePressForward = () => {
+    seek?.current?.(currentTime + FORWARD_TIME, 10);
+    videoPlayerStore$.controlPanelAwaked.set(false);
+  };
+
+  const handlePressBackward = () => {
+    seek?.current?.(currentTime - BACKWARD_TIME, 10);
+    videoPlayerStore$.controlPanelAwaked.set(false);
+  };
+
   return {
     handlePressPlay,
     handlePressPause,
     isPlaying,
+    handlePressForward,
+    handlePressBackward,
   };
 }
