f104e8f4883fcd6f32947efdc70e346b32b0dc17
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-29 01:02:42 +0200

    UPDATE: - Add initialText parameter to useDebouncedHandler

--- src/core/hooks/useDebouncedTextHandler.tsx
+++ src/core/hooks/useDebouncedTextHandler.tsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from 'react';
 export const useDebouncedHandler = ({
   callback,
   delay = 300,
+  initialText = '',
 }: {
   callback: (text: string) => void;
   delay?: number;
+  initialText?: string;
 }) => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState(initialText);
 
   useEffect(() => {
     const handler = setTimeout(() => {
