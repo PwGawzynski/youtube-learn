e667ba3733b06d77842543e7dc23299bea878da4
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-28 09:30:36 +0200

    REFACTOR: - Updated TopControlls component to include subtitle button and improved layout for better alignment of control buttons.

--- src/features/video-player/components/TopControlls.tsx
+++ src/features/video-player/components/TopControlls.tsx
@@ -4,21 +4,31 @@ import { useTopControlls } from '../hooks/useTopControlls';
 import { ControllButton } from './ControllButton';
 
 export function TopControlls() {
-  const { handlePressMute, handlePressPip } = useTopControlls();
+  const { handlePressMute, handlePressPip, handlePressSubtitle } =
+    useTopControlls();
   return (
-    <View className="w-full flex-row items-center justify-end gap-2">
-      <ControllButton
-        // eslint-disable-next-line global-require
-        icon={require('../assets/volume-icon.svg')}
-        onPress={handlePressMute}
-      />
+    <View className="w-full flex-row items-center justify-between gap-2">
+      <View className="flex-row items-center gap-2 ">
+        <ControllButton
+          // eslint-disable-next-line global-require
+          icon={require('../assets/languages.svg')}
+          onPress={handlePressSubtitle}
+        />
+      </View>
+      <View className="flex-row items-center gap-2 ">
+        <ControllButton
+          // eslint-disable-next-line global-require
+          icon={require('../assets/volume-icon.svg')}
+          onPress={handlePressMute}
+        />
 
-      {/* pip-button */}
-      <ControllButton
-        // eslint-disable-next-line global-require
-        icon={require('../assets/airplay-icon.svg')}
-        onPress={handlePressPip}
-      />
+        {/* pip-button */}
+        <ControllButton
+          // eslint-disable-next-line global-require
+          icon={require('../assets/airplay-icon.svg')}
+          onPress={handlePressPip}
+        />
+      </View>
     </View>
   );
 }
