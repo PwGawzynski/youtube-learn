fa8f9969420241209760df096975533adc8831aa
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-29 01:11:49 +0200

    UPDATE: - Refactor SearchResults component to include sort prop and integrate useSearchResults hook

--- src/features/search/components/SearchResults.tsx
+++ src/features/search/components/SearchResults.tsx
@@ -1,14 +1,17 @@
 import { VideoCard } from '@/components/shared/videoCard/VideoCard';
 import { VideoList } from '@/components/shared/videoList/VideoList';
 
+import { useSearchResults } from '../hooks/useSearchResults';
 import type { SearchResultsProps } from '../types/components-types';
 
 const ITEM_WIDTH = 350;
 const ITEM_HEIGHT = 300;
 const PADDING_TOP = 16;
-export function SearchResults({ data, isLoading }: SearchResultsProps) {
+export function SearchResults({ data, isLoading, sort }: SearchResultsProps) {
+  const { onPress } = useSearchResults();
   return (
     <VideoList
+      extraData={{ sort }}
       isLoading={isLoading}
       vertical={{
         estimatedItemSize: ITEM_WIDTH,
@@ -17,6 +20,7 @@ export function SearchResults({ data, isLoading }: SearchResultsProps) {
       data={data}
       renderItem={({ item }) => (
         <VideoCard
+          onPress={onPress}
           showChannelTitle
           item={item}
           style={{ width: ITEM_WIDTH, height: ITEM_HEIGHT }}
