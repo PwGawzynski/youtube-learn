3b5e8bf9540591fa94980a377673fd760628a725
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-28 14:08:12 +0200

    ADD: - Added videoId prop to ProgressBarProps and VideoPlayerProps

--- src/features/video-player/types/component-types.ts
+++ src/features/video-player/types/component-types.ts
@@ -8,4 +8,10 @@ export type ControllButtonProps = {
 
 export type ControlPanelProps = {};
 
-export type ProgressBarProps = {};
+export type ProgressBarProps = {
+  videoId: string;
+};
+
+export type VideoPlayerProps = {
+  videoId: string;
+};
