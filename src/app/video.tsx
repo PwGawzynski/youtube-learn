5b4a4f8215b155605cfe91a42cad0121f0aeb701
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-28 10:58:44 +0200

    ADD: - add Header component and state management for segmented control

--- src/app/video.tsx
+++ src/app/video.tsx
@@ -1,8 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { useLocalSearchParams } from 'expo-router';
+import { useState } from 'react';
 import { SafeAreaView, View } from 'react-native';
 
 import { ChanelProfile } from '@/components/shared/chanelProfile/ChanelProfile';
+import { Header } from '@/components/shared/Header';
 import { SegmentedControl } from '@/components/shared/segmentedControl';
 import { getVideos } from '@/core/api/services/videos/videos';
 import type { VideoScreenLocalParams } from '@/core/types';
@@ -12,6 +14,7 @@ import { VideoPlayer } from '@/features/video-player';
 export default function Video() {
   const { videoId } = useLocalSearchParams<VideoScreenLocalParams>();
 
+  const [activeIndex, setActiveIndex] = useState(0);
   const { data } = useQuery({
     queryKey: ['video', 'statistics, snippet', videoId],
     queryFn: () => getVideos({ id: videoId, part: 'statistics, snippet' }),
@@ -19,19 +22,25 @@ export default function Video() {
     staleTime: Infinity,
   });
 
+  const title = data?.items[0].snippet.title;
+  const chanelName = data?.items[0].snippet.channelTitle;
   return (
     <SafeAreaView className="mt-12 flex-1">
       <View className="h-1/3 w-full">
         <VideoPlayer />
       </View>
+      <View className="w-full px-4 py-2">
+        <Header className="text-2xl font-bold">{title}</Header>
+      </View>
       <View className="h-2/3 w-full px-4">
-        <ChanelProfile chanelName={data?.items[0].snippet.channelTitle} />
+        <ChanelProfile chanelName={chanelName} />
         <SegmentedControl
           segments={['Details', 'Notes']}
           defaultActiveIndex={0}
-          onChange={() => {}}
+          onChange={setActiveIndex}
         />
-        <VideoDetails video={data?.items[0]} />
+        {activeIndex === 0 && <VideoDetails video={data?.items[0]} />}
+        {activeIndex === 1 && <View />}
       </View>
     </SafeAreaView>
   );
