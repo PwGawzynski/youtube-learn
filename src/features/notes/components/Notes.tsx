45b406ba0c9fc71d49f1cf428d57fbf8c4c26e8c
pwgawzynski <pgawzynski.backup@gmail.com>
2025-04-28 13:59:54 +0200

    ADD: - Notes component for managing and displaying notes associated with a video

---
+++ src/features/notes/components/Notes.tsx
@@ -0,0 +1,20 @@
+import { KeyboardAvoidingView, Platform } from 'react-native';
+
+import { useNotes } from '../hooks/useNotes';
+import type { NotesProps } from '../types/components-types';
+import { NoteInput } from './NoteInput';
+import { NotesList } from './NotesList';
+
+export function Notes({ videoId }: NotesProps) {
+  const notes$ = useNotes({ videoId });
+
+  return (
+    <KeyboardAvoidingView
+      className="flex-1"
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <NotesList notes$={notes$} />
+      <NoteInput notes$={notes$} />
+    </KeyboardAvoidingView>
+  );
+}
